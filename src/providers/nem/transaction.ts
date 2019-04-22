cd7ce094f1039b04da05e9da7adbfcd36838f8d6
Linus Kreitner <linus.kreitner@tum.de>
2019-04-22 16:28:36 +0200

    add(nem/transaction): Added nem transaction calls

---
+++ src/providers/nem/transaction.ts
@@ -0,0 +1,78 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {
+  Account, Address, Deadline, UInt64, NetworkType, PlainMessage,
+  TransferTransaction, Mosaic, MosaicId, TransactionHttp, AggregateTransaction
+} from 'nem2-sdk';
+import { AccountProvider } from '../account/account';
+/*
+  Generated class for the NemTransactionProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class NemTransactionProvider {
+
+  private network = NetworkType.TEST_NET;
+  private networkURL = 'http://localhost:3000'; //Have to check on this
+  private mosaicID = '7cdf3b117a3c40cc'; // Replace with our mosaicId
+  private acc;
+
+  constructor(public http: HttpClient, public account: AccountProvider) {
+    console.log('Hello NemTransactionProvider Provider');
+    this.acc = Account.createFromPrivateKey(account.getPrivateKey(), this.network);
+  }
+
+  public createTransAction(receipients: string[], title: string, amount: number) {
+    if (receipients.length == 1) {
+      this.singleTransaction(receipients[0], title, amount);
+    } else {
+      this.aggregateTransaction(receipients, title, amount);
+    }
+  }
+
+  private singleTransaction(receipient: string, title: string, amount: number) {
+    //Create Tx
+    let transferTransaction = TransferTransaction.create(
+      Deadline.create(),
+      Address.createFromRawAddress(receipient),
+      [new Mosaic(new MosaicId(this.mosaicID), UInt64.fromUint(amount))], 
+      PlainMessage.create(title),
+      this.network
+    );
+
+    //Sign
+    let signedTransaction = this.acc.sign(transferTransaction);
+
+    //Announce to network
+    const transactionHttp = new TransactionHttp(this.networkURL);
+    transactionHttp.announce(signedTransaction).subscribe(
+      x => console.log("Successfully completed transaction! "+x),
+      err => console.log(err)
+    );
+  }
+
+  private aggregateTransaction(receipients: string[], title: string, amount: number) {
+    //Create txs
+    let txs = [];
+    for(let receipient of receipients){
+      let tx = TransferTransaction.create(Deadline.create(),
+      Address.createFromRawAddress(receipient),
+      [new Mosaic(new MosaicId(this.mosaicID), UInt64.fromUint(amount))],
+      PlainMessage.create('title'), this.network);
+      tx.toAggregate(this.acc.publicAccount);
+      txs.push(tx);
+    }
+    const aggregateTransaction = AggregateTransaction.createComplete(Deadline.create(), txs, this.network, []);
+
+    //Sign
+    const signedTransaction = this.acc.sign(aggregateTransaction);
+    
+    //Announce to network
+    const transactionHttp = new TransactionHttp(this.networkURL);
+    transactionHttp.announce(signedTransaction).subscribe(
+      x => console.log("Successfully completed transactions! "+x), 
+      err => console.error(err));
+  }
+}
